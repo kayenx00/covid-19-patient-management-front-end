d409764eae1a9c7ea288b2286b80e8cdce7d70bb
Nguyễn Hoàng Long <Nguyenhlong0910@gmail.com>
2022-09-11 21:13:41 +0700

    Update

--- src/Patient/ViewDoctorForRegister/DoctorList.jsx
+++ src/Patient/ViewDoctorForRegister/DoctorList.jsx
@@ -1,10 +1,145 @@
 import React, { useState, useEffect } from 'react';
-function DoctorList() {
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+function DoctorList({filteredDoctors}) {
+    let navigate = useNavigate()
+    const filtered = filteredDoctors
+    console.log(filtered)
+    const [dataLimit, setDataLimit] = useState(6)
+    const numberOfDoctors = filtered.length
+    const pageLimit = Math.ceil(numberOfDoctors/dataLimit)
+    const [pages] = useState(Math.floor(numberOfDoctors / dataLimit));
+    const [currentPage, setCurrentPage] = useState(1);
+    const handleBack=()=>{
+        const doctor_id = localStorage.getItem('user_id')
+        navigate('/Doctor/'+ doctor_id)
+    }
+    const handleView = (id) => {
+        localStorage.setItem('patient_id', id)
+        navigate("/DoctorViewPatientDeclaration/" + id)
+    } 
+    function goToNextPage() {
+
+        setCurrentPage((page) => page + 1);
+
+     }
+   
+     function goToPreviousPage() {
+
+        setCurrentPage((page) => page - 1);
+
+     }
+   
+     function changePage(event) {
+
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
+
+     }
+   
+     const getPaginatedData = () => {
+        const startIndex = currentPage * dataLimit - dataLimit;
+        const endIndex = startIndex + dataLimit;
+        return filtered.slice(startIndex, endIndex);
+     };
+   
+     const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+
+      };
+    const handleDecreaseSongLimit = () => {
+        setDataLimit( prevState => prevState - 1)
+    }
+
+    const handleIncreaseSongLimit = () => {
+        setDataLimit( prevState => prevState + 1)
+    }
     return (
         <div>
-            
+            <div className = 'rows'>
+                <table className = "table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>ID Number</th>
+                            <th>Phone</th>
+                            <th>City</th>
+                            <th>District</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+{
+                            getPaginatedData().map(f =>
+                            
+                                <tr key ={f.id}>
+                                    <td>{f.id}</td>
+                                    <td>{f.name}</td>
+                                    <td>{f.id_num}</td>
+                                    <td>{f.phone}</td>
+                                    <td>{f.city}</td>
+                                    <td>{f.district}</td>
+                                    <td>
+                                        <button onClick = {() => handleView(f.id)}>View Patient Declaration</button>
+                                    </td>
+
+                                </tr>
+                                
+                            )
+                        }
+
+                        <tr>
+                            <td>
+                                Number of result Doctors: {numberOfDoctors}
+                            </td>
+                            <td>
+                            <button
+                                disabled = {(currentPage === 1)}
+                                onClick={goToPreviousPage}
+                                className={`prev ${currentPage === 1 ? 'disabled' : ''}`}>
+                                    Prev
+                            </button>
+                            {getPaginationGroup().map((item, index) => (
+                            <button
+                                key={index}
+                                onClick={changePage}
+                                className={`paginationItem ${currentPage === item ? 'active' : null}`}>
+                                <span>{item}</span>
+                            </button>
+                            
+                            ))}
+                            <button
+                            disabled = {(currentPage === pageLimit)}
+                            onClick={goToNextPage}
+                            className={`next ${currentPage === pages ? 'disabled' : ''}`}>
+                                Next
+                            </button>
+                            </td>
+                            <td>
+                                Set Doctor Record Limit: 
+                                    <button disabled = {dataLimit === 1} onClick = {e => handleDecreaseSongLimit()}>
+                                        -
+                                    </button> 
+                                    {dataLimit}
+                                    <button disabled = {dataLimit === numberOfDoctors} onClick = {e => handleIncreaseSongLimit()}>
+                                        +
+                                    </button> 
+                            </td>
+                            <td>
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button onClick = {()=>handleBack()}>Back</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    );
+    )
 }
 
 export default DoctorList;
--- src/Patient/ViewDoctorForRegister/SearchDoctor.jsx
+++ src/Patient/ViewDoctorForRegister/SearchDoctor.jsx
@@ -1,10 +1,56 @@
 import React, { useState, useEffect } from 'react';
-function SearchDoctor() {
+import DoctorList from '../../Admin/AdminUtils/forDoctor/viewDoctor/DoctorList';
+function SearchDoctor({details}) {
+    const [searchField, setSearchField] = useState("")
+    const doctors = details
+    console.log(doctors);
+    const filteredDoctors = doctors.filter(
+        doctor => {
+          return (
+            doctor
+            .name
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+            ||
+            doctor
+            .id_num
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+            ||            
+            doctor
+            .phone
+            .toLowerCase()
+            .includes(searchField.toLowerCase())
+          );
+        }
+      );
+    const handleChange = (e) =>{
+        setSearchField(e.target.value);
+    }
+    function searchList() {
+      if(patients.length === 0){
+        return(
+          <div>
+            <h2 className='text-center'> There are no Doctors available</h2>
+          </div>
+        )
+      }
+      else{
+        return (
+            <DoctorList filteredDoctors={filteredDoctors} />
+        );}
+      }
     return ( 
         <div>
-            
+            <input 
+            className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+            type = "search" 
+            placeholder = "Search Patient" 
+            onChange = {handleChange}
+            />
+            {searchList()}
         </div>
-     );
+    );
 }
 
 export default SearchDoctor;
